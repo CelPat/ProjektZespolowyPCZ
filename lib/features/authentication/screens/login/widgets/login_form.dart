2b88da6fa2c3c85781c262c65ba61d4ce4846af5
pcelka <pcelka@griddynamics.com>
2025-01-08 14:37:27 +0100

    resolve conflcits and remove unused imports in login_form

--- lib/features/authentication/screens/login/widgets/login_form.dart
+++ lib/features/authentication/screens/login/widgets/login_form.dart
@@ -1,15 +1,10 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-<<<<<<< HEAD
-import 'package:projekt_zespolowy_pcz/features/authentication/screens/signup/signup.dart';
-=======
-import 'package:get/get_core/src/get_main.dart';
-import 'package:projekt_zespolowy_pcz/features/authentication/screens/home/home.dart';
->>>>>>> ac293054ee9e2488c4d4f5406f973786174e6633
-
 import '../../../../../utils/constants/sizes.dart';
 import '../../../../../utils/constants/text_strings.dart';
+import '../../../../guided_meditations/screens/home/home.dart';
+import '../../signup/signup.dart';
 
 class ZenLoginForm extends StatelessWidget {
   const ZenLoginForm({
@@ -58,9 +53,7 @@ class ZenLoginForm extends StatelessWidget {
             SizedBox(
               width: double.infinity,
               child: ElevatedButton(
-                onPressed: 
-                  () => Get.offAll(() => const HomePage())
-                ,
+                onPressed: () => Get.offAll(() => const HomePage()),
                 child: const Text(ZenTexts.signIn),
               ),
             ),
