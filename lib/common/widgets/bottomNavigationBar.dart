ac293054ee9e2488c4d4f5406f973786174e6633
pcelka <pcelka@griddynamics.com>
2025-01-08 14:29:30 +0100

    change home screens location. Fix naming an text_strings

--- lib/common/widgets/bottomNavigationBar.dart
+++ lib/common/widgets/bottomNavigationBar.dart
@@ -1,17 +1,16 @@
 import 'package:flutter/material.dart';
 
-class bottomNavigationBar extends StatelessWidget {
-  const bottomNavigationBar({
+class ZenBottomNavigationBar extends StatelessWidget {
+  const ZenBottomNavigationBar({
     super.key,
   });
 
   @override
   Widget build(BuildContext context) {
     return BottomNavigationBar(items: const <BottomNavigationBarItem>[
-              BottomNavigationBarItem(
-      icon: Icon(Icons.home_outlined), label: 'Home'),
-              BottomNavigationBarItem(
-      icon: Icon(Icons.person_2_outlined), label: 'Profile')
-            ]);
+      BottomNavigationBarItem(icon: Icon(Icons.home_outlined), label: 'Home'),
+      BottomNavigationBarItem(
+          icon: Icon(Icons.person_2_outlined), label: 'Profile')
+    ]);
   }
 }
---
+++ lib/features/guided_meditations/screens/home/home.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+import 'package:projekt_zespolowy_pcz/utils/constants/image_strings.dart';
+import 'package:projekt_zespolowy_pcz/utils/constants/sizes.dart';
+import 'package:projekt_zespolowy_pcz/utils/constants/text_strings.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Center(
+          child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Image.asset(ZenImages.appLogo, fit: BoxFit.cover),
+                const SizedBox(
+                  height: ZenSizes.spaceBetweenSections,
+                ),
+                ElevatedButton(
+                  onPressed: () {},
+                  child: const Text(ZenTexts.meditations),
+                ),
+                const SizedBox(
+                  height: ZenSizes.spaceBetweenItems,
+                ),
+                ElevatedButton(
+                  onPressed: () {},
+                  child: const Text(ZenTexts.helpCenter),
+                ),
+                const SizedBox(
+                  height: ZenSizes.spaceBetweenItems,
+                ),
+                ElevatedButton(
+                  onPressed: () {},
+                  child: const Text(ZenTexts.knowledgeCenter),
+                ),
+              ]),
+        ),
+        bottomNavigationBar:
+            BottomNavigationBar(items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+              icon: Icon(Icons.home_outlined), label: 'Home'),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.person_2_outlined), label: 'Profile')
+        ]));
+  }
+}
