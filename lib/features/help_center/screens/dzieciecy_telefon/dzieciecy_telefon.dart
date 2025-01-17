337a79d3ca087fca544eefe208552ce5b2e1f345
pcelka <pcelka@griddynamics.com>
2025-01-17 21:07:36 +0100

    Fix app bar icon theme. Update DzieciecyTelefon page

--- lib/features/help_center/screens/dzieciecy_telefon/dzieciecy_telefon.dart
+++ lib/features/help_center/screens/dzieciecy_telefon/dzieciecy_telefon.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:projekt_zespolowy_pcz/utils/constants/sizes.dart';
 import 'package:projekt_zespolowy_pcz/utils/constants/text_strings.dart';
+import 'package:url_launcher/link.dart';
+import 'package:url_launcher/url_launcher_string.dart';
 
 class DzieciecyTelefon extends StatelessWidget {
   const DzieciecyTelefon({super.key});
@@ -8,35 +10,61 @@ class DzieciecyTelefon extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
+          title: const Text(ZenTexts.dzieciecyTelefonZaufaniaTitle),
+          centerTitle: true,
           leading: BackButton(
             onPressed: () {
               Navigator.pop(context);
             },
           ),
+          iconTheme: Theme.of(context).iconTheme,
         ),
-        body: SingleChildScrollView(
+        body: Padding(
+            padding: const EdgeInsets.all(20),
             child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            const SizedBox(child: Text(ZenTexts.dzieciecyTelefonZaufaniaTitle)),
-            const SizedBox(
-              height: ZenSizes.spaceBetweenItems,
-            ),
-            SizedBox(
-              child: ElevatedButton(
-                  onPressed: () {}, child: const Text('800 12 12 12')),
-            ),
-            const SizedBox(
-              height: ZenSizes.spaceBetweenItems,
-            ),
-            const SizedBox(
-                child: Text(
-                    'Pod tym numerem dzieci, rodzice i opiekunowie mogą uzyskać wsparcie psychologiczne i prawne. Dorośli mogą zgłaszać problemy dzieci lub zaniedbania. Najczęściej poruszane są sprawy przemocy, problemów emocjonalnych, relacji rówieśniczych, szkolnych i rodzinnych. Dostępny jest także całodobowy chat.')),
-            const SizedBox(
-              height: ZenSizes.spaceBetweenItems,
-            ),
-            const SizedBox(child: Text('800121212.pl')),
-          ],
-        )));
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                const SizedBox(
+                  height: ZenSizes.spaceBetweenItems,
+                ),
+                SizedBox(
+                    child: ElevatedButton.icon(
+                  onPressed: () {
+                    launchUrlString('tel:+48 800 12 12 12');
+                  },
+                  style:
+                      ElevatedButton.styleFrom(fixedSize: const Size(150, 60)),
+                  icon: const Icon(
+                    Icons.phone_rounded,
+                    color: Colors.white,
+                  ),
+                  label: const Text('800 12 12 12'),
+                )
+
+                    // ElevatedButton(
+                    //     onPressed: () {}, child: const Text('800 12 12 12')),
+                    ),
+                const SizedBox(
+                  height: ZenSizes.spaceBetweenItems * 2,
+                ),
+                const SizedBox(
+                    child: Text(
+                        'Pod tym numerem dzieci, rodzice i opiekunowie mogą uzyskać wsparcie psychologiczne i prawne. Dorośli mogą zgłaszać problemy dzieci lub zaniedbania. Najczęściej poruszane są sprawy przemocy, problemów emocjonalnych, relacji rówieśniczych, szkolnych i rodzinnych. Dostępny jest także całodobowy chat.',
+                        textAlign: TextAlign.center)),
+                const SizedBox(
+                  height: ZenSizes.spaceBetweenItems,
+                ),
+                SizedBox(
+                    //Text('800121212.pl')
+                    width: 150,
+                    child: Link(
+                        uri: Uri.parse('https://800121212.pl/'),
+                        builder: (context, followLink) {
+                          return ElevatedButton(
+                              onPressed: followLink,
+                              child: const Text('800121212.pl'));
+                        })),
+              ],
+            )));
   }
 }
--- lib/features/help_center/screens/telefon_dzieci_i_mlodziezy/telefon_dzieci_i_mlodziezy.dart
+++ lib/features/help_center/screens/telefon_dzieci_i_mlodziezy/telefon_dzieci_i_mlodziezy.dart
@@ -8,7 +8,7 @@ import 'package:url_launcher/url_launcher_string.dart';
 
 class TelefonDzieciIMlodziezy extends StatelessWidget {
   const TelefonDzieciIMlodziezy({super.key});
-  
+
 //   makingPhoneCall() async {
 //   var url = Uri.parse("tel:116111");
 //   if (await canLaunchUrl(url)) {
@@ -29,60 +29,57 @@ class TelefonDzieciIMlodziezy extends StatelessWidget {
               Navigator.pop(context);
             },
           ),
+          iconTheme: Theme.of(context).iconTheme,
         ),
         body: Padding(
             padding: const EdgeInsets.all(20),
             child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            const SizedBox(
-              height: ZenSizes.spaceBetweenItems,
-            ),
-            SizedBox(
-              child: 
-              // ElevatedButton(
-              //     onPressed: () {},
-              //     style: ElevatedButton.styleFrom(
-              //       minimumSize: const Size(200, 40),
-              //     ), 
-              //     child: const Text('116 111')
-              //     ),
-              ElevatedButton.icon(
-                onPressed: () {
-                  launchUrlString('tel:+48 116 111');
-                },
-                style: ElevatedButton.styleFrom(
-                  fixedSize: const Size(150, 60)
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                const SizedBox(
+                  height: ZenSizes.spaceBetweenItems,
                 ),
-                icon: const Icon(
-                  Icons.phone_rounded,
-                  color: Colors.white,
-                  ), 
-                label: const Text('116 111'),
-                )
-            ),
-            const SizedBox(
-              height: ZenSizes.spaceBetweenItems * 2,
-            ),
-            const SizedBox(
-                child: Text(
-                    'Telefon ten wspiera dzieci i młodzież w trudnych sytuacjach, umożliwiając rozmowę o ważnych sprawach, problemach, emocjach i doświadczeniach. Można dzwonić w przypadku przemocy, dyskryminacji, problemów rodzinnych, szkolnych, rówieśniczych lub osobistych, a także w chwilach niepokoju czy potrzeby wsparcia.',
-                    textAlign: TextAlign.center)),
-            const SizedBox(
-              height: ZenSizes.spaceBetweenItems * 2,
-            ),
-            SizedBox(
-              width: 150,
-              child: Link(
-                uri: Uri.parse('https://116111.pl/'), 
-                builder: (context, followLink){
-                  return ElevatedButton(
-                    onPressed: followLink, 
-                    child: const Text('116111.pl'
-                    ));
-                }) 
-              ),
-          ],
-        )));
+                SizedBox(
+                    child:
+                        // ElevatedButton(
+                        //     onPressed: () {},
+                        //     style: ElevatedButton.styleFrom(
+                        //       minimumSize: const Size(200, 40),
+                        //     ),
+                        //     child: const Text('116 111')
+                        //     ),
+                        ElevatedButton.icon(
+                  onPressed: () {
+                    launchUrlString('tel:+48 116 111');
+                  },
+                  style:
+                      ElevatedButton.styleFrom(fixedSize: const Size(150, 60)),
+                  icon: const Icon(
+                    Icons.phone_rounded,
+                    color: Colors.white,
+                  ),
+                  label: const Text('116 111'),
+                )),
+                const SizedBox(
+                  height: ZenSizes.spaceBetweenItems * 2,
+                ),
+                const SizedBox(
+                    child: Text(
+                        'Telefon ten wspiera dzieci i młodzież w trudnych sytuacjach, umożliwiając rozmowę o ważnych sprawach, problemach, emocjach i doświadczeniach. Można dzwonić w przypadku przemocy, dyskryminacji, problemów rodzinnych, szkolnych, rówieśniczych lub osobistych, a także w chwilach niepokoju czy potrzeby wsparcia.',
+                        textAlign: TextAlign.center)),
+                const SizedBox(
+                  height: ZenSizes.spaceBetweenItems * 2,
+                ),
+                SizedBox(
+                    width: 150,
+                    child: Link(
+                        uri: Uri.parse('https://116111.pl/'),
+                        builder: (context, followLink) {
+                          return ElevatedButton(
+                              onPressed: followLink,
+                              child: const Text('116111.pl'));
+                        })),
+              ],
+            )));
   }
 }
